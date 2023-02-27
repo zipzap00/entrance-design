972bfbb472164c354e3bcd4cf7a50eac4290d8ac
Your Name <halikow.b@yandex.ru>
2023-02-27 14:38:46 +0300

    add icons from input

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -1,22 +1,33 @@
-const FormWrapper = () => {
-  return (
+import {AiOutlineMail} from 'react-icons/ai';
+import {AiOutlineLock} from 'react-icons/ai';
+const FormWrapper = () => (
+  
   <div className="flex justify-center m-32">
-    <div className="bg-slate-400 w-96 h-80 rounded-md" >
+    <div className="bg-slate-400 w-96 h-80 rounded-md">
       <div className="text-center  p-7 font-medium text-lg">
-      Вход в Yoldi Agency
-      </div> 
-      <div className="text-center">
-        <input className="w-80 h-12 rounded-md outline-none" type="email" placeholder="E-mail"/>
+        Вход в Yoldi Agency
       </div>
-      <div className="text-center  py-4">
-        <input className="w-80 h-12 rounded-md outline-none" type="email" placeholder="Пароль"/>
+      
+      <div className="relative">
+        <div className="flex items-center px-20">
+          <AiOutlineMail className='w-5 h-5 absolute ml-3'/>
+          <input className="w-80 h-12 rounded-md outline-none pr-3 pl-10" type="email" placeholder="E-mail" />
+        </div>
       </div>
+
+    
+      <div className="relative py-4">
+        <div className="flex items-center px-20">
+          <AiOutlineLock className='w-5 h-5 absolute ml-3'/>
+          <input className="w-80 h-12 rounded-md outline-none pr-3 pl-10" type="email" placeholder="Пароль" />
+        </div>
+      </div>
+
       <div className="text-center">
         <button className="w-80 h-12 rounded-md bg-slate-200">Войти</button>
       </div>
     </div>
   </div>
-  );
-}
+)
 
 export default FormWrapper;
